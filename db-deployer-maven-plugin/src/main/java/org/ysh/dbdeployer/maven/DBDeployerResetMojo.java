a3181cc6b0d0f7bab1431a80898df0b7eb59b5c0
yangshuo <yangshuosdu@gmail.com>
2018-07-05 21:43:48 +0800

    release 1.0.0

--- db-deployer-maven-plugin/src/main/java/org/ysh/dbdeployer/maven/DBDeployerResetMojo.java
+++ db-deployer-maven-plugin/src/main/java/org/ysh/dbdeployer/maven/DBDeployerResetMojo.java
@@ -1,7 +1,5 @@
 package org.ysh.dbdeployer.maven;
 
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.ysh.dbdeployer.api.DBDeployException;
 
